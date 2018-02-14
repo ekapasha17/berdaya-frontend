73b7011d2673e961619d3921f3bc5ead46b8b4f3
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-02-14 17:19:31 +0700

    :joy:

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,12 @@
 <template>
-<div id="app">
-<template id="step1" v-if="currentStep == 1">
-<h1>Step 1</h1>
-
-<div>
-<input type="text" name="name" v-model="step1.name" placeholder="name">
-</div>
-<div>
-<input type="text" name="email" v-model="step1.email" placeholder="email">
-</div>
-
-<button type="button" @click.prevent="goToStep(2)">Continue</button>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
-<template id="step2" v-if="currentStep == 2">
-<h1>Step 2</h1>
 
-<div>
-<input type="text" name="city" v-model="step2.city" placeholder="city">
-</div>
-<div>
-<input type="text" name="state" v-model="step2.state" placeholder="state">
-</div>
-
-<button type="button" @click.prevent="goToStep(3)">Finish</button>
-</template>
-<template id="step3" v-if="currentStep == 3">
-<strong>Name:</strong> {{ step1.name }}<br />
-<strong>Email:</strong> {{ step1.email }}<br />
-<strong>City:</strong> {{ step2.city }}<br />
-<strong>State:</strong> {{ step2.state }}
-</template>
-</div>
-</template>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
-<script src="assets/jquery-2.1.4.min.js"></script>
 <script>
 export default {
   name: 'App'
 }
 </script>
-
-<script type = "text/javascript">
-Vue.component('step1', {
-  template: '#step1',
-  props: [
-      'currentStep',
-      'step1'
-  ]
-});
-
-Vue.component('step2', {
-  template: '#step2',
-  props: [
-      'currentStep',
-      'step2'
-  ]
-});
-
-Vue.component('step3', {
-  template: '#step3',
-  props: [
-      'currentStep',
-      'step1',
-      'step2'
-  ]
-});
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        currentStep: 1,
-        step1: {
-            name: '',
-            email: ''
-        },
-        step2: {
-            city: '',
-            state: ''
-        }
-    },
-    ready: function() {
-        console.log('ready');
-    },
-    methods: {
-        goToStep: function(step) {
-            this.currentStep = step;
-        }
-    }
-});
-</script>
-
-<style>
-</style>
