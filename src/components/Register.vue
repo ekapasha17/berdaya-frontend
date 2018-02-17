dda150e049d05297a3e115480b501b1da7ec0223
ekapasha17 <pashaeka@gmail.com>
2018-02-17 16:13:34 +0700

    show hide has done

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,30 +1,25 @@
 <template>
   <div id="register">
-    <template id="step1" v-if="currentStep == 1">
+    <template id="step1" v-if="!seen">
       <h1>Step 1</h1>
       <div>
-        <input type="text" name="name" v-model="step1.name" placeholder="name">
+        <input type="text" name="name" placeholder="name">
       </div>
       <div>
-        <input type="text" name="email" v-model="step1.email" placeholder="email">
+        <input type="text" name="email" placeholder="email">
       </div>
-      <button type="button" @click.prevent="goToStep(2)">Continue</button>
+      <button type="button" v-on:click="seen = !seen">Continue</button>
     </template>
-    <template id="step2" v-if="currentStep == 2">
+    <template id="step2" v-if="seen">
       <h1>Step 2</h1>
       <div>
-        <input type="text" name="city" v-model="step2.city" placeholder="city">
+        <input type="text" name="city" placeholder="city">
       </div>
       <div>
-        <input type="text" name="state" v-model="step2.state" placeholder="state">
+        <input type="text" name="state" placeholder="state">
       </div>
-      <button type="button" @click.prevent="goToStep(3)">Finish</button>
-    </template>
-    <template id="step3" v-if="currentStep == 3">
-      <strong>Name:</strong> {{ step1.name }}<br />
-      <strong>Email:</strong> {{ step1.email }}<br />
-      <strong>City:</strong> {{ step2.city }}<br />
-      <strong>State:</strong> {{ step2.state }}
+      <button v-on:click="seen = !seen" type="button">Back</button>
+      <button type="button">Finish</button>
     </template>
   </div>
 </template>
@@ -32,42 +27,17 @@
 <script>
 import Vue from 'vue'
 export default {
-  name: 'Register'
+  name: 'Register',
+  data () {
+    return {
+      seen: false
+    }
+  }
 }
-Vue.component('step1', {
-  template: '#step1',
-  props: [
-    'currentStep',
-    'step1'
-  ]
-})
-
-Vue.component('step2', {
-  template: '#step2',
-  props: [
-    'currentStep',
-    'step2'
-  ]
-})
-
-Vue.component('step3', {
-  template: '#step3',
-  props: [
-    'currentStep',
-    'step1',
-    'step2'
-  ]
-})
 
 var app = new Vue({
-  el: '#register',
-  data: {
-    currentStep: 1,
-    step1: {
-      name: '',
-      email: ''
-    },
-    message: ''
-  }
+  el: '#register'
 })
+
+console.log(app)
 </script>
